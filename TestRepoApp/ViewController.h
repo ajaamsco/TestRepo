ddc37a2e25a1ebbf42973792bb85911c17f4956e
ajaamsco <angusj@aamsco.com.au>
2015-11-30 15:01:27 +1100

    initial commit

---
+++ TestRepoApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestRepoApp
+//
+//  Created by Angus Johnston on 30/11/2015.
+//  Copyright © 2015 Angus Johnston. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TestRepoApp/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TestRepoApp
+//
+//  Created by Angus Johnston on 30/11/2015.
+//  Copyright © 2015 Angus Johnston. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+	[super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+	
+	//********* !!!! WOOOOHOOOOOOoooooooo
+}
+
+- (void)didReceiveMemoryWarning {
+	[super didReceiveMemoryWarning];
+	// Dispose of any resources that can be recreated.
+}
+
+@end
